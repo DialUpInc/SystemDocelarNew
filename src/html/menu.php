ed7e487ef58e90f26ef94bfd4cafd0e5c326e4f2
Marcos Vinicius <vinicius_potato@hotmail.com>
2020-06-04 18:38:11 -0300

    Doce Lar cadastros

---
+++ src/html/menu.php
@@ -0,0 +1,86 @@
+<aside class="left-sidebar" data-sidebarbg="skin6">
+<div class="scroll-sidebar" data-sidebarbg="skin6">
+<!-- Sidebar navigation-->
+<nav class="sidebar-nav">
+    <ul id="sidebarnav">
+        <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="index.php"
+                aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
+                    class="hide-menu">Inicio</span></a></li>
+        <li class="list-divider"></li>
+
+        <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                aria-expanded="false"><i class="feather-icon icon-user-follow"></i><span
+                    class="hide-menu"> Cadastro </span></a>
+            <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                <li class="sidebar-item"><a href="cad_cliente.php" class="sidebar-link"><span
+                            class="hide-menu"> Cliente
+                        </span></a>
+                </li>
+                <li class="sidebar-item"><a href="cad_funcionario.php" class="sidebar-link"><span
+                            class="hide-menu"> Funcionarios
+                        </span></a>
+                </li>
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu"> Fornecedores
+                        </span></a>
+                </li>
+
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu"> Cadastro de Materiais
+                        </span></a>
+                </li>
+            </ul>
+        </li>
+        <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                aria-expanded="false"><i class="feather-icon icon-notebook"></i><span
+                    class="hide-menu">Orçamento </span></a>
+            <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu"> Gerar Orçamento
+                        </span></a>
+                </li>
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu"> Ver Orçamentos
+                        </span></a>
+                </li>
+            </ul>
+        </li>
+        <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                aria-expanded="false"><i data-feather="bar-chart" class="feather-icon"></i><span
+                    class="hide-menu">Financeiro </span></a>
+            <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu"> Contas a Pagar
+                        </span></a>
+                </li>
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu"> Contas a Receber
+                        </span></a>
+                </li>
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu">
+                            Boleto
+                        </span></a>
+                </li>
+            </ul>
+        </li>
+        <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                aria-expanded="false"><i data-feather="box" class="feather-icon"></i><span
+                    class="hide-menu">Estoque </span></a>
+            <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                <li class="sidebar-item"><a href="#" class="sidebar-link"><span
+                            class="hide-menu"> Materiais
+                        </span></a>
+                </li>
+            </ul>
+        </li>
+        <!-- <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="ui-cards.html"
+                aria-expanded="false"><i data-feather="sidebar" class="feather-icon"></i><span
+                    class="hide-menu">Cards
+                </span></a>
+        </li> -->
+    </ul>
+</nav>
+<!-- End Sidebar navigation -->
+</div>
+</aside>
